965096a0c76eda942bda18759a05018cdb265681
Santiago Ramirez <santiago.ramirezc26@gmail.com>
2022-05-28 10:38:05 -0500

    Inicialize Angular project using differents ways to transfer the data between the component and the view

---
+++ my-store_Angular/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  name = 'Santiago';
+  age = 22;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  person = {
+    name: 'Rambo',
+    age: 50,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+}
